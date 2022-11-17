02ddfd692768c13ca41b5891765010b37ce5304e
oscar anillo <oscarani14@gmail.com>
2022-11-17 13:05:04 -0500

    add keep track component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import HeaderComponent from "./components/header-component";
 
 import "./App.css";
+import KeepTrack from "./components/keep-track-component";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <HeaderComponent />
+        <KeepTrack />
       </div>
     </div>
   );
